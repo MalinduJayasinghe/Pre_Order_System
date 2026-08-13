6ba9ea12d7cbfa9cf0a8cd134ed7e2c88f63f014
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-08-13 13:00:12 +0530

    added WebConfig for image uploads

---
+++ src/main/java/lk/ijse/preordersystem/security/WebConfig.java
@@ -0,0 +1,14 @@
+package lk.ijse.preordersystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:uploads/menu-images/");
+    }
+}
