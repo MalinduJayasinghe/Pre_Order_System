5680c7ce660ace1b81a74ff2a2ca2c72b6eb22e0
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-09-07 18:20:29 +0530

    added FavoriteMenuItem entity

---
+++ src/main/java/lk/ijse/preordersystem/entity/FavoriteMenuItem.java
@@ -0,0 +1,29 @@
+package lk.ijse.preordersystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FavoriteMenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long favoriteId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private MenuItem menuItem;
+
+    private LocalDateTime addedAt;
+}
