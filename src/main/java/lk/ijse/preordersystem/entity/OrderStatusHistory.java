96b62f14c97ba3c6746e82d619eb7556ba991070
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-09-07 18:27:33 +0530

    added OrderStatusHistory

---
+++ src/main/java/lk/ijse/preordersystem/entity/OrderStatusHistory.java
@@ -0,0 +1,27 @@
+package lk.ijse.preordersystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderStatusHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long historyId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private String status;
+    private LocalDateTime changedAt;
+    private String changedBy;
+}
