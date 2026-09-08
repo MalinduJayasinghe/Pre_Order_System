8f680e4e505c2dfc143b699ed87cc5a94e230114
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-09-08 09:04:44 +0530

    added DiscountRepository

---
+++ src/main/java/lk/ijse/preordersystem/repository/DiscountRepository.java
@@ -0,0 +1,13 @@
+package lk.ijse.preordersystem.repository;
+
+import lk.ijse.preordersystem.entity.Discount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DiscountRepository extends JpaRepository<Discount,Long> {
+
+    Optional<Discount> findByCodeIgnoreCase(String code);
+}
