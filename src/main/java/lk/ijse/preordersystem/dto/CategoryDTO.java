45491f382a3e36f0ddd87e168ecb590dd04c594c
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-09-08 09:18:46 +0530

    added CategoryDTO

---
+++ src/main/java/lk/ijse/preordersystem/dto/CategoryDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.preordersystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+
+    private long categoryId;
+    private String categoryName;
+    private String description;
+}
