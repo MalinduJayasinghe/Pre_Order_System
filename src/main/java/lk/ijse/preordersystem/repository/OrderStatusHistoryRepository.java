05e2b7453222f18b3780f39646a932589218f33f
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-09-08 09:09:07 +0530

    added OrderStatusHistoryRepository

---
+++ src/main/java/lk/ijse/preordersystem/repository/OrderStatusHistoryRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.preordersystem.repository;
+
+import lk.ijse.preordersystem.entity.OrderStatusHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderStatusHistoryRepository extends JpaRepository<OrderStatusHistory,Long> {
+}
