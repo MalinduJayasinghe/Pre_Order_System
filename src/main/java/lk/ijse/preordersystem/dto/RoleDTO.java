1efa2f9595dbf054026646762ca3f78c5cda310e
MalinduJayasinghe <jayasinghemalindu3@gmail.com>
2026-09-08 09:29:06 +0530

    added RoleDTO

---
+++ src/main/java/lk/ijse/preordersystem/dto/RoleDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.preordersystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDTO {
+
+    private long roleId;
+    private String roleName;
+    private String description;
+}
